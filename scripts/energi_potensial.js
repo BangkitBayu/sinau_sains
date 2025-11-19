ed00a72a49a2c58978a41a024488603bea7941c8
BangkitBayu <martinusmartinussaja@gmail.com>
2025-11-19 22:11:19 +0700

    feat: nambah halaman fisika dan energi potensial

---
+++ scripts/energi_potensial.js
@@ -0,0 +1,36 @@
+let epChart; // menampung instance chart
+
+function generateChart() {
+  const m = parseFloat(document.getElementById("weight").value);
+  const g = parseFloat(document.getElementById("gravity").value);
+  const h = parseFloat(document.getElementById("height").value);
+
+  const Ep = m * g * h;
+
+  const ctx = document.getElementById("epChart").getContext("2d");
+
+  // jika chart sudah ada, hapus dulu supaya tidak double
+  if (epChart) epChart.destroy();
+
+  epChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Energi Potensial (Joule)"],
+      datasets: [
+        {
+          label: "Ep",
+          data: [Ep],
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
+
+
