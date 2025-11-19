29f85abe2dabf59d41d4044a6968610b8fa643a4
BangkitBayu <martinusmartinussaja@gmail.com>
2025-11-19 22:51:03 +0700

    feat: nambah halaman gerak lurus beraturan

---
+++ scripts/glb.js
@@ -0,0 +1,34 @@
+let glbChart;
+
+function generateChart() {
+  const d = parseFloat(document.getElementById("distance").value);
+  const t = parseFloat(document.getElementById("time").value);
+
+  const Glb = d / t;
+
+  const ctx = document.getElementById("glbChart").getContext("2d");
+
+  if (glbChart) glbChart.destroy();
+
+  glbChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Kecepatan (m/s)"],
+      datasets: [
+        {
+          label: "V",
+          data: [Glb],
+        },
+      ],
+    },
+    options: {
+      indexAxis: "y",
+      scales: {
+        x: {
+          title: { display: true, text: "waktu (s)" },
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+}
